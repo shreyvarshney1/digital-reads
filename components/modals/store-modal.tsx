fd51fce6e03c213fa1a7b2033c3d15388fc7c656
Shrey5908 <shreyvarshney1@gmail.com>
2024-10-31 17:44:42 +0530

    fix: admin redirect

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -43,7 +43,7 @@ export const StoreModal = () => {
 
       const response = await axios.post('/api/stores', values);
 
-      window.location.assign(`/${response.data.id}`);
+      window.location.assign(`/admin/${response.data.id}`);
     } catch (error) {
       toast.error('Something went wrong.');
     } finally {
